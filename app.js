4f720ec4d72d7654d33e7cde454c56536f73df5d
Dele O <dele@authoritywit.com>
2021-07-06 11:48:55 -0400

    add a simple fetch
    This currently pulls the data from a local JSON file to illustrate the base workings of the `fetch` API

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("It's working");
+const fetchInput = 'github.json';
+
+//@TODO: play with GH's API {}
+//@TODO: build our app with an API...
+
+// > block 1
+fetch(fetchInput)
+    .then(res => res.json())
+    .then(content => {
+        console.log(content);
+    });
+
+// > block 2
+const myData = 'some string'; //string
+
+// >  block 3
+console.log(myData);
